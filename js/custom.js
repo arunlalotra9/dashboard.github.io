60f02b4cb883ee0b327d94b62dacb671203eccb3
AtulRana89 <atulrana076@gmail.com>
2023-05-17 14:41:20 +0530

    username

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+//  Multi Dropdown
+    function initMultiDropdown() {
+        $('.dropdown-menu a.dropdown-toggle').on('click', function () {
+            var dropdown = $(this).next('.dropdown-menu');
+            var otherDropdown = $(this).parent().parent().find('.dropdown-menu').not(dropdown);
+            otherDropdown.removeClass('show')
+            otherDropdown.parent().find('.dropdown-toggle').removeClass('show')
+            return false;
+        });
+    }
